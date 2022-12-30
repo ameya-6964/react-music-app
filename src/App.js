a5bb0d98be2c491ba23fab72e262f190a5eb1650
ameya-6964 <belvalkar7@gmail.com>
2022-12-30 21:56:08 +0530

    Added Styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/app.scss'
 //! Adding Components
 import Player from './components/Player'
 import Song from './components/Song'
+import Library from './components/Library';
 
 //! Import util 
 import data from './util';
@@ -17,6 +18,7 @@ const App = () => {
     <div>
       <Song currentSong={currentSong} />
       <Player isPlaying={isPlaying}  setIsPlaying={setIsPlaying} currentSong={currentSong} />
+      <Library songs={songs} />
     </div>
   )
 }
